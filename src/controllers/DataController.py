2d78de26adff5f9657b26856171a62d958722246
Ahmed_KKhaled <zakreahmed666@gmail.com>
2026-08-28 10:20:20 +0300

    applying the generate_unique_filepath to avoid conflicts

--- src/controllers/DataController.py
+++ src/controllers/DataController.py
@@ -1,4 +1,6 @@
 from .BaseController import BaseController
+from .ProjectController import ProjectController
+import os
 from fastapi import UploadFile
 from models import ResponseSignal
 
@@ -19,3 +21,28 @@ class DataController(BaseController):
             return False, ResponseSignal.FILE_SIZE_EXCEDDED.value
 
         return True, ResponseSignal.FILE_VALIDATED_SUCCESS.value
+
+    def generate_unique_filepath(self, orig_file_name: str, project_id: str):
+
+        random_key = self.generate_random_string()
+        project_path = ProjectController().get_project_path(project_id=project_id)
+
+        cleaned_file_name = self.get_clean_file_name(
+            orig_file_name=orig_file_name
+        )
+
+        new_file_path = os.path.join(
+            project_path,
+            random_key + "_" + cleaned_file_name
+        )
+
+        while os.path.exists(new_file_path):
+            random_key = self.generate_random_string()
+            new_file_path = os.path.join(
+                project_path,
+                random_key + "_" + cleaned_file_name
+            )
+
+        return new_file_path, random_key + "_" + cleaned_file_name
+
+
