4e0d114445f32bd956355f6752ef3624b2db2640
Ahmed_KKhaled <zakreahmed666@gmail.com>
2026-08-28 09:37:53 +0300

    create project controller.py

--- src/routes/data.py
+++ src/routes/data.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, APIRouter, Depends, UploadFile
+from fastapi import FastAPI, APIRouter, Depends, UploadFile, status
+from fastapi.responses import JSONResponse
 import os
 from helpers.config import get_settings, Settings
 from controllers import DataController
@@ -16,7 +17,13 @@ async def upload_data(project_id: str, file: UploadFile,
     # validate the file extensions
     data_controller = DataController()
 
-    is_valid = data_controller.validate_uploaded_file(file=file)
+    is_valid, signal = data_controller.validate_uploaded_file(file=file)
 
-    return is_valid
+    if not is_valid:
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content = {
+                "signal": signal
+            }
+        )
         
