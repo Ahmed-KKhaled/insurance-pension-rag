2bcb1e839f5d18f8673f188a0b151299b019f6a4
Ahmed_KKhaled <zakreahmed666@gmail.com>
2026-08-28 07:51:40 +0300

    creating Enums directory and Responsesignal Enum and included them in the data controller

--- src/controllers/DataController.py
+++ src/controllers/DataController.py
@@ -1,6 +1,6 @@
 from .BaseController import BaseController
 from fastapi import UploadFile
-import logging
+from models import ResponseSignal
 
 
 class DataController(BaseController):
@@ -8,17 +8,14 @@ class DataController(BaseController):
     def __init__(self):
         super().__init__()
         self.size_scale = 1048576 # convert MB to bytes
-        self.logger = logging.getLogger(__name__)
 
 
     def validate_uploaded_file(self, file: UploadFile) -> bool:
 
         if file.content_type not in self.app_settings.FILE_ALLOWED_EXTENSIONS:
-            self.logger.error(f"only file types allowed are {self.app_settings.FILE_ALLOWED_EXTENSIONS}")
-            return False
+            return False, ResponseSignal.FILE_TYPE_NOT_SUPPORTED.value
 
         if file.size > self.app_settings.FILE_MAX_SIZE * self.size_scale:
-            self.logger.error(f"max size allowed is {self.app_settings.FILE_MAX_SIZE}")
-            return False
+            return False, ResponseSignal.FILE_SIZE_EXCEDDED.value
 
-        return True
+        return True, ResponseSignal.FILE_VALIDATED_SUCCESS.value
---
+++ src/models/Enums/ResponseEnums.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+
+class ResponseSignal(Enum):
+
+    FILE_VALIDATED_SUCCESS = "file validated success"
+    FILE_TYPE_NOT_SUPPORTED = "file type not supported"
+    FILE_SIZE_EXCEDDED = "file size excedded"
+    FILE_UPLOADED_SUCCESS = 'file uploaded success'
+    FILE_UPLOADED_FAIL = 'file uploaded fail'
+    
