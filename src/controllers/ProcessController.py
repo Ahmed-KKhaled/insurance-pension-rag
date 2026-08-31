e44a935eb9d9f6afdabd380976e90b403e51de97
Ahmed_KKhaled <zakreahmed666@gmail.com>
2026-08-31 09:59:46 +0300

    add get_file_content function

--- src/controllers/ProcessController.py
+++ src/controllers/ProcessController.py
@@ -18,9 +18,13 @@ class ProcessController(BaseController):
 
         return file_ext
 
-    def get_file_loader(self, file_id: str, file_path: str):
+    def get_file_loader(self, file_id: str):
 
         file_ext = self.get_file_extension(file_id=file_id)
+        file_path = os.path.join(
+            self.project_path,
+            file_id
+        )
 
         if file_ext == ProcessingEnum.TXT.value:
             return TextLoader(file_path, encoding="utf-8")
@@ -29,6 +33,12 @@ class ProcessController(BaseController):
             return PyMuPDFLoader(file_path)
 
         return None
+
+    def get_file_content(self, file_id: str):
+
+        loader = self.get_file_loader(file_id=file_id)
+
+        return loader.load()
 
 
 
