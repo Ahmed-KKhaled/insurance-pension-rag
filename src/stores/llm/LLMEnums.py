8f0682005c9332e913190b2de52e60955d9e5571
Ahmed_KKhaled <zakreahmed666@gmail.com>
2026-09-06 18:10:22 +0300

    Add HuggingFace Provider

--- src/stores/llm/LLMEnums.py
+++ src/stores/llm/LLMEnums.py
@@ -20,6 +20,12 @@ class CohereEnums(Enum):
     DOCUMENT = "search_document"
     QUERY = "search_query"
 
+class HuggingfaceEnum(Enum):
+    SYSTEM = "system"
+    USER = "user"
+    ASSISTANT = "assistant"
+
 class DocumentTypeEnum(Enum):
     DOCUMENT = "document"
     QUERY = "query"
+
