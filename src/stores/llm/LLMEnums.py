731461e0dee7a4ac1fca7d6e84b673ea7478bf33
Ahmed_KKhaled <zakreahmed666@gmail.com>
2026-09-06 15:47:50 +0300

    update LLMEnums with Cohere enums

--- src/stores/llm/LLMEnums.py
+++ src/stores/llm/LLMEnums.py
@@ -10,3 +10,12 @@ class OpenAIEnums(Enum):
     SYSTEM = "system"
     USER = "user"
     ASSISTANT = "assistant"
+
+class CohereEnums(Enum):
+    
+    SYSTEM = "SYSTEM"
+    USER = "USER"
+    ASSISTANT = "CHATBOT"
+
+    DOCUMENT = "search_document"
+    QUERY = "search_query"
