3de0f3bfa17852b3b0b681fc234d2b0c4899ed2e
Ahmed_KKhaled <zakreahmed666@gmail.com>
2026-09-07 14:51:34 +0300

    Include vector db in the main.py and add get_database_path function in basecontroller

--- src/stores/vectordb/VectorDBProviderFactory.py
+++ src/stores/vectordb/VectorDBProviderFactory.py
@@ -1,17 +1,21 @@
 from .providers.QdrantProvider import QdrantProvider
 from .VectorDBEnums import VectorDBEnums
+from controllers import BaseController
 
 class VectorDBProviderFactory:
     def __init__(self, config: dict):
         self.config = config
 
-
+        self.base_controller = BaseController()
 
     def create(self, provider: str):
 
+        db_path = self.base_controller.get_database_path(
+            db_name=self.config.VECTOR_DB_PATH
+        )
         if provider == VectorDBEnums.QDRANT.value:
             return QdrantProvider(
-                db_path=self.config.VECTOR_DB_PATH,
+                db_path=db_path,
                 distance_method=self.config.VECTOR_DB_DISTANCE_METHOD
             )
 
