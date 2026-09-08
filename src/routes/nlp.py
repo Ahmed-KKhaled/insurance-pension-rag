91100cd29d220e86d0de77bcd720fc98c589e222
Ahmed_KKhaled <zakreahmed666@gmail.com>
2026-09-08 11:40:59 +0300

    Add nlp routers

---
+++ src/routes/nlp.py
@@ -0,0 +1,164 @@
+from fastapi import FastAPI, APIRouter, status, Request
+from fastapi.responses import JSONResponse
+from .schemes import PushRequest, SearchRequest
+from models import ProjectModel, ChunkModel
+from controllers import NLPController
+from models.Enums import ResponseSignal
+import logging
+
+logger = logging.getLogger("uvicorn.error")
+
+
+nlp_router = APIRouter(
+    prefix = "/api/v1/nlp",
+    tags=["api_v1", "nlp"]
+)
+
+@nlp_router.post("/index/push/{project_id}")
+async def index_project(request: Request, project_id: str, push_request: PushRequest):
+
+    project_model = await ProjectModel.create_instance(
+             db_client=request.app.db_client
+        )
+    project = await project_model.get_project_or_create_one(project_id=project_id)
+
+    if not project:
+        return JSONResponse(
+                    status_code=status.HTTP_400_BAD_REQUEST,
+                    content = {
+                        "signal": ResponseSignal.PROJECT_NOT_FOUND_ERROR.value
+                    }
+                )
+
+    chunk_model = await ChunkModel.create_instance(
+        db_client=request.app.db_client
+    )
+
+    
+    nlp_controller = NLPController(
+        vectordb_client=request.app.vectordb_client,
+        generation_client=request.app.generation_client,
+        embedding_client=request.app.embedding_client
+    )
+
+    has_records = True
+    page_no = 1
+    inserted_chunks_into_vdb = 0
+
+    while has_records:
+        page_chunks = await chunk_model.get_chunks_by_project_id(
+                project_id=project.id,
+                page_no=page_no
+        )
+
+        if len(page_chunks):
+            page_no += 1
+
+
+        if not page_chunks or len(page_chunks) == 0:
+            has_records = False
+            break
+
+        is_inserted = nlp_controller.index_into_vector_db(
+                project=project,
+                chunks=page_chunks,
+                do_reset=push_request.do_reset
+            )
+
+        if not is_inserted:
+            return JSONResponse(
+                        status_code=status.HTTP_400_BAD_REQUEST,
+                        content = {
+                            "signal": ResponseSignal.INSERT_INTO_VECTOR_DB_ERROR.value
+                        }
+                    )
+        
+        inserted_chunks_into_vdb += len(page_chunks)
+
+
+    return JSONResponse(
+                    content = {
+                        "signal": ResponseSignal.INSERT_INTO_VECTOR_DB_SUCCESS.value,
+                        "inserted_chunks" : inserted_chunks_into_vdb
+                    }
+                )
+
+
+@nlp_router.get("/index/info/{project_id}")
+async def get_project_index_info(request: Request, project_id: str):
+
+    project_model = await ProjectModel.create_instance(
+                 db_client=request.app.db_client
+            )
+    project = await project_model.get_project_or_create_one(project_id=project_id)
+    
+    if not project:
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content = {
+                "signal": ResponseSignal.PROJECT_NOT_FOUND_ERROR.value
+            }
+        )
+
+    
+    nlp_controller = NLPController(
+        vectordb_client=request.app.vectordb_client,
+        generation_client=request.app.generation_client,
+        embedding_client=request.app.embedding_client
+    )
+
+    collection_info = nlp_controller.get_vector_collection_info(
+        project=project
+    )
+
+    return JSONResponse(
+        content={
+            "signal": ResponseSignal.VECTORDB_COLLECTION_RETRIEVED.value,
+            "collection_info": collection_info.model_dump()
+        }
+    )
+
+@nlp_router.post("/index/search/{project_id}")
+async def search_index(request: Request, project_id: str, search_request: SearchRequest):
+
+   
+   project_model = await ProjectModel.create_instance(
+                    db_client=request.app.db_client
+    )
+   project = await project_model.get_project_or_create_one(project_id=project_id)
+    
+   if not project:
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content = {
+                "signal": ResponseSignal.PROJECT_NOT_FOUND_ERROR.value
+            }
+        )
+
+    
+   nlp_controller = NLPController(
+        vectordb_client=request.app.vectordb_client,
+        generation_client=request.app.generation_client,
+        embedding_client=request.app.embedding_client
+    )
+
+   results = nlp_controller.search_vector_db_collection(
+        project=project,
+        text=search_request.text,
+        limit=search_request.limit
+    )
+
+   if not results:
+        return JSONResponse(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                content={
+                    "signal": ResponseSignal.VECTORDB_SEARCH_ERROR.value
+                }
+            )
+    
+   return JSONResponse(
+        content={
+            "signal": ResponseSignal.VECTORDB_SEARCH_SUCCESS.value,
+            "results": [ result.dict()  for result in results ]
+        }
+    )
--- src/stores/llm/LLMProviderFactory.py
+++ src/stores/llm/LLMProviderFactory.py
@@ -24,7 +24,7 @@ class LLMProviderFactory:
             return CohereProvider(
                 api_key = self.config.COHERE_API_KEY,
                 default_input_max_characters=self.config.INPUT_DAFAULT_MAX_CHARACTERS,
-                default_generation_max_output_tokens=self.config.GENERATION_DAFAULT_MAX_TOKENS,
+                default_generation_output_max_tokens=self.config.GENERATION_DAFAULT_MAX_TOKENS,
                 default_generation_temperature=self.config.GENERATION_DAFAULT_TEMPERATURE
             )
 
@@ -40,7 +40,7 @@ class LLMProviderFactory:
             return SentenceTransformerProvider(
                 default_input_max_character=self.config.INPUT_DAFAULT_MAX_CHARACTERS,
                 default_generation_max_output_tokens=self.config.GENERATION_DAFAULT_MAX_TOKENS,
-                default_generation_temperature=self.config.GENERATION_DAFAULT_TEMPERATURE
+                temperature=self.config.GENERATION_DAFAULT_TEMPERATURE
             )
 
         return None
