9476b02e088d884974aa2924be6114e2667a1123
Ahmed khaled <zakreahmed666@gmail.com>
2026-09-08 17:21:14 +0300

    Update config.py with new env template config

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -35,6 +35,9 @@ class Settings(BaseSettings):
     VECTOR_DB_PATH: str
     VECTOR_DB_DISTANCE_METHOD: str=None
 
+    PRIMARY_LANG:str=None
+    DEFAULT_LANG:str="en"
+
     class Config:
         env_file=".env"
 
