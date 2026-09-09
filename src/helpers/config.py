ec2af5ad40505b89136fc3c8181dda9f43ed64c4
Ahmed khaled <zakreahmed666@gmail.com>
2026-09-09 22:00:18 +0300

    update env values

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -1,5 +1,5 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
-
+from typing import List
 
 class Settings(BaseSettings):
 
@@ -19,9 +19,9 @@ class Settings(BaseSettings):
     POSTGRES_PORT:int
     POSTGRES_MAIN_DATABASE:str
 
-    GENERATION_BACKEND_LITERAL: str=None
+    GENERATION_BACKEND_LITERAL: List[str]=None
     GENERATION_BACKEND: str
-    EMBEDDING_BACKEND_LITERAL: str=None
+    EMBEDDING_BACKEND_LITERAL: List[str]=None
     EMBEDDING_BACKEND: str
 
     OPENAI_API_KEY: str=None
@@ -36,7 +36,7 @@ class Settings(BaseSettings):
     GENERATION_DAFAULT_MAX_TOKENS: int=None
     GENERATION_DAFAULT_TEMPERATURE: float=None
 
-    VECTOR_DB_BACKEND_LITERAL: str=None
+    VECTOR_DB_BACKEND_LITERAL: List[str]=None
     VECTOR_DB_BACKEND: str
     VECTOR_DB_PATH: str
     VECTOR_DB_DISTANCE_METHOD: str=None
