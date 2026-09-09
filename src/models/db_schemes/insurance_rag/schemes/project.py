078e03ceb48bdfe67196c8e9fdf44359af777f3e
Ahmed khaled <zakreahmed666@gmail.com>
2026-09-09 13:29:31 +0300

    Add asset init scheme + relationship

--- src/models/db_schemes/insurance_rag/schemes/project.py
+++ src/models/db_schemes/insurance_rag/schemes/project.py
@@ -14,3 +14,8 @@ class Project(SQLAlchemyBase):
 
     created_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
     updated_at = Column(DateTime(timezone=True), onupdate=func.now(), nullable=True)
+
+    assets = relationship(
+        "Asset",
+        back_populates="project"
+    )
