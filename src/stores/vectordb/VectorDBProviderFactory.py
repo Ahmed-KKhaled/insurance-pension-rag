5f78447fe7efb06c9f104d6933e62baf5bf367af
Ahmed khaled <zakreahmed666@gmail.com>
2026-09-10 18:48:32 +0300

    Add PGVector provider and standardize provider parameters

--- src/stores/vectordb/VectorDBProviderFactory.py
+++ src/stores/vectordb/VectorDBProviderFactory.py
@@ -1,31 +1,56 @@
-from .providers import QdrantProvider, ChromaDBProvider
+from .providers import QdrantProvider, ChromaDBProvider, PGVectorProvider
 from .VectorDBEnums import VectorDBEnums
 from controllers import BaseController
+from sqlalchemy.orm import sessionmaker
 
 class VectorDBProviderFactory:
-    def __init__(self, config: dict):
-        self.config = config
+    def __init__(self, config, db_client: sessionmaker=None):
 
+        self.config = config
         self.base_controller = BaseController()
+        self.db_client = db_client
 
     def create(self, provider: str):
 
-        db_path = self.base_controller.get_database_path(
-            db_name=self.config.VECTOR_DB_PATH
-        )
-        
         if provider == VectorDBEnums.QDRANT.value:
-            return QdrantProvider(
-                db_path=db_path,
-                distance_method=self.config.VECTOR_DB_DISTANCE_METHOD
+            qdrant_db_client=self.base_controller.get_database_path(db_name=self.config.VECTOR_DB_PATH)
+
+
+            provider = QdrantProvider(
+                db_client=qdrant_db_client,
+                distance_method=self.config.VECTOR_DB_DISTANCE_METHOD,
+                default_vector_size=self.config.EMBEDDING_MODEL_SIZE,
+                index_threshold=self.config.VECTOR_DB_PGVEC_INDEX_THRESHOLD
+
             )
 
-        if provider == VectorDBEnums.CHROMA.value:
-            return ChromaDBProvider(
-                db_client=db_path,
-                distance_method=self.config.VECTOR_DB_DISTANCE_METHOD
-            ) 
+            return provider
 
+
+        if provider == VectorDBEnums.CHROMA.value:
+            chroma_db_client=self.base_controller.get_database_path(db_name=self.config.VECTOR_DB_PATH)
+
+            provider = ChromaDBProvider(
+                  db_client=chroma_db_client,
+                  distance_method=self.config.VECTOR_DB_DISTANCE_METHOD,
+                  default_vector_size=self.config.EMBEDDING_MODEL_SIZE,
+                  index_threshold=self.config.VECTOR_DB_PGVEC_INDEX_THRESHOLD
+            )
+
+            return provider
+        
+
+        if provider == VectorDBEnums.PGVECTOR.value:
+
+            provider = PGVectorProvider(
+                db_client=self.db_client,
+                distance_method=self.config.VECTOR_DB_DISTANCE_METHOD,
+                default_vector_size=self.config.EMBEDDING_MODEL_SIZE,
+                index_threshold=self.config.VECTOR_DB_PGVEC_INDEX_THRESHOLD
+            )
+
+            return provider
+        
 
         return None
         
