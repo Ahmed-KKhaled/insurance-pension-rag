192cbc86343ff77690abe1cb94dbd5c2528f3dbb
Ahmed khaled <zakreahmed666@gmail.com>
2026-09-13 18:42:44 +0300

    Add Conversation Scheme

---
+++ src/models/db_schemes/insurance_rag/schemes/conversation.py
@@ -0,0 +1,31 @@
+from .insurance_rag_base import SQLAlchemyBase
+from sqlalchemy import Column, Integer, DateTime, func, String, ForeignKey
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import relationship
+import uuid
+
+class Conversation(SQLAlchemyBase):
+
+    __tablename__ = "conversations"
+
+    conversation_id = Column(Integer, primary_key=True, autoincrement=True)
+    conversation_uuid = Column(UUID(as_uuid=True), default=uuid.uuid4,  unique=True, nullable=False)
+
+    title = Column(String, nullable=False)
+
+    created_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now(), nullable=True)
+
+    conversation_project_id = Column(Integer, ForeignKey("projects.project_id"), nullable=False)
+
+    project = relationship(
+        "Project",
+        back_populates="conversations"
+    )
+
+    messages = relationship(
+        "Message",
+        back_populates="conversation"
+    )
+
+
