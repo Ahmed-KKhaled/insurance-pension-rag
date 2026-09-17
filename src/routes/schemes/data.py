8c97ecdc63bd54d7ab832c4618ad98855b3dba7f
Ahmed khaled <zakreahmed666@gmail.com>
2026-09-17 23:18:22 +0300

    Add metadata to asset_config

--- src/routes/schemes/data.py
+++ src/routes/schemes/data.py
@@ -6,3 +6,13 @@ class ProcessRequest(BaseModel):
     chunk_size: Optional[int] = 100
     overlap_size: Optional[int] = 20
     do_reset: Optional[int] = 0
+
+
+Asset_config={
+        "document_type": "law",
+        "law_number": "148",
+        "law_year": "2019",
+        "title": "قانون التأمينات الاجتماعية والمعاشات",
+        "source": "قانون التأمينات الاجتماعية والمعاشات رقم 148 لسنة 2019",
+        "language": "ar",
+    }
