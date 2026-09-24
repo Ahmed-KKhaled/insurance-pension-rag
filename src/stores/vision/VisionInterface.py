4558f4d86f99f6e1feaf675a6614556558614657
Ahmed khaled <zakreahmed666@gmail.com>
2026-09-25 00:31:18 +0300

    Update VisionInterface with extract_text_from_image

--- src/stores/vision/VisionInterface.py
+++ src/stores/vision/VisionInterface.py
@@ -4,7 +4,14 @@ from abc import ABC, abstractmethod
 class VisionInterface(ABC):
 
     @abstractmethod
-    async def extract(self, image: list[str]) -> str:
+    async def extract_text_from_table(self, image: list[str]) -> str:
         pass
+
+
+    @abstractmethod
+    async def extract_text_from_image(self, image: str) -> str:
+        pass
+
+
 
     
